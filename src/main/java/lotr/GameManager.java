2dba1742ea43507eb3ebb5ea2931f8744bb252a8
Danylo Tsymbal <d.tsymbal.pn@ucu.edu.ua>
2026-07-10 01:30:37 +0300

    fix: prevent infinite fight loop, extract shared random-damage logic
    Extract shared random-damage logic into RandomFighter, exclude abstract
    classes from factory scan. Prevent infinite loop in fight by adding a
    turn limit and draw condition.

--- src/main/java/lotr/GameManager.java
+++ src/main/java/lotr/GameManager.java
@@ -1,10 +1,14 @@
 package lotr;
 
 public class GameManager {
+    private static final int MAX_TURNS = 100;
+
     public void fight(Character c1, Character c2) {
         System.out.println(c1 + " vs. " + c2);
+        int turns = 0;
 
-        while (c1.isAlive() && c2.isAlive()) {
+        while (c1.isAlive() && c2.isAlive() && turns < MAX_TURNS) {
+            turns++;
             System.out.println(c1.getClass().getSimpleName() + " attacks " + c2.getClass().getSimpleName());
             c1.kick(c2);
             System.out.println(c2.getClass().getSimpleName() + " has " + c2.getHp() + " HP left.");
@@ -23,15 +27,15 @@ public class GameManager {
             }
         }
 
-        System.out.println("The fight is over!");
+        System.out.println(turns >= MAX_TURNS
+            ? "The fight ended in a draw after " + MAX_TURNS + " turns."
+            : "The fight is over!");
     }
+
     public static void main(String[] args) {
         CharacterFactory factory = new CharacterFactory();
         Character c1 = factory.createCharacter();
         Character c2 = factory.createCharacter();
-
-        GameManager game = new GameManager();
-        game.fight(c1, c2);
+        new GameManager().fight(c1, c2);
     }
 }
-
